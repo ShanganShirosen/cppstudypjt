28860dcbf12fbf292fc5d1371d23268590e19ee5
上官 白宣 <118045774+ShanganShirosen@users.noreply.github.com>
2022-11-14 12:17:01 +0800

    1
    1

--- main.cpp
+++ main.cpp
@@ -2,8 +2,42 @@
  
 int main()
 {
-    /* 我的第一个 C 程序 */
     printf("Hello, World! \n");
  
     return 0;
+}
+struct student
+
+{
+
+    int x;
+
+    char c;
+
+} a;
+
+ 
+
+int main()
+
+{
+
+    a.x=3;
+
+    a.c='a';
+
+    f(a);
+
+    printf("%d,%c",a.x,a.c);
+
+}
+
+f(struct student b)
+
+{
+
+    b.x=20;
+
+    b.c='y';
+
 }
